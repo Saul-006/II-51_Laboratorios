0dc40b5f66643c6715267d94b769190a31ff967e
Saul-006 <saul2006r3@gmail.com>
2026-04-23 01:48:56 -0600

    scxfd
    fdsfdf

--- Lab08/vite.config.js
+++ Lab08/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/II-51_Laboratorios/Lab08/dist/'
+  base: './II-51_Laboratorios/Lab08/dist/'
 })
